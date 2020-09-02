41e061e953fab0c7a68aa90bc6003fee4e420c18
Kevin Vance <kvance@kvance.com>
2020-09-02 16:45:16 -0400

    This could work

--- GLS29EE512_programmer.ino
+++ GLS29EE512_programmer.ino
@@ -32,6 +32,8 @@ typedef struct {
   uint8_t device;
 } ProductId;
 
+char inputBuffer[128];
+
 void setup() {
   // Set address pins to output zeros
   DDR_ADDR_LOW = 0xff;
@@ -45,26 +47,83 @@ void setup() {
   setDataReadMode();
 
   Serial.begin(57600);
-}
-
-void loop() {
-  char buf[128];
 
   ProductId productId = getProductId();
+  char buf[32];
   sprintf(buf, "Manufacturer: %02X, product: %02X", productId.manufacturer, productId.device);
   Serial.println(buf);
   if(productId.manufacturer != 0xbf || productId.device != 0x5d) {
-    Serial.println("Expected BF/5D, cowardly refusing all further commands!");
-    while(true) { delay(1000); }
-  }
+    while(true) {
+      Serial.println("Expected BF/5D, cowardly refusing all further commands!");
+      readSerialCommand(inputBuffer, sizeof(inputBuffer));
+      Serial.println(buf);
+    }
+  }  
+}
 
-  char data[128];
-  for(int i = 0; i < 128; i++) {
-    data[i] = 128 + i;
-  }
-  Serial.println("Writing page 1");
-  writePage(128, data);
+void loop() {
+  Serial.print("ready>");
+  readSerialCommand(inputBuffer, sizeof(inputBuffer));
+  Serial.println();
 
+  // Parse command
+  int cmdEnd = findTokenEnd(inputBuffer, sizeof(inputBuffer), 0);
+  if(cmdEnd == 0) {
+    return;
+  }
+  if(!strncmp("help", inputBuffer, cmdEnd)) {
+    printHelp();
+    return;
+  }
+  if(!strncmp("write", inputBuffer, cmdEnd)) {
+    int page;
+    if(!parseNumberArgument(inputBuffer, sizeof(inputBuffer), cmdEnd, &page)) {
+      return;
+    }
+    if(page < 0 || page >= 512) {
+      Serial.println("Page must be between 0 and 511 inclusive");
+      return;
+    }
+    Serial.print("INPUT 128 BYTES#");
+    Serial.setTimeout(1000 * 60);  // 1 minute
+    size_t bytesRead = Serial.readBytes(inputBuffer, 128);
+    if(bytesRead != 128) {
+      Serial.println("Timed out waiting for 128 bytes");
+      return;
+    }    
+    writePage(128 * page, inputBuffer);
+    Serial.println("OK");
+    return;
+  }
+  if(!strncmp("verify", inputBuffer, cmdEnd)) {
+    int page;
+    if(!parseNumberArgument(inputBuffer, sizeof(inputBuffer), cmdEnd, &page)) {
+      return;
+    }
+    Serial.print("INPUT 128 BYTES#");
+    Serial.setTimeout(1000 * 60);  // 1 minute
+    size_t bytesRead = Serial.readBytes(inputBuffer, 128);
+    if(bytesRead != 128) {
+      Serial.println("Timed out waiting for 128 bytes");
+      return;
+    }    
+    if(verifyPage(128 * page, inputBuffer)) {
+      Serial.println("OK");
+    } else {
+      Serial.println("FAIL");
+    }
+  }
+  if(!strncmp("hexdump", inputBuffer, cmdEnd)) {
+    hexDump();
+    return;
+  }
+  Serial.println("Unrecognized command");
+  return;
+}
+
+void hexDump() {
+  delay(10);
+  char buf[128];
   setDataReadMode();
   PORT_CTRL = CHIP_ENABLED | OUTPUT_ENABLED | WRITE_DISABLED;
   for(int row = 0; row < 64; row++) {
@@ -96,8 +155,8 @@ ProductId getProductId() {
   PORT_CTRL = CHIP_ENABLED | OUTPUT_ENABLED | WRITE_DISABLED;
   // Read id bytes
   ProductId result;
-  result.manufacturer = readAddr(0x0000);
-  result.device = readAddr(0x0001);
+  result.manufacturer = slowReadAddr(0x0000);
+  result.device = slowReadAddr(0x0001);
 
   PORT_CTRL = CHIP_ENABLED | OUTPUT_DISABLED | WRITE_DISABLED;
   setDataWriteMode();
@@ -163,9 +222,17 @@ inline void writePulse() {
 inline uint8_t readAddr(uint16_t address) {
   PORT_ADDR_HIGH = address >> 8;
   PORT_ADDR_LOW = address & 0xff;
-  delayMicroseconds(2);  // no idea why I have to wait so long...
   return PIN_DATA;
 }
+
+// Required for reading the device ID
+inline uint8_t slowReadAddr(uint16_t address) {
+  PORT_ADDR_HIGH = address >> 8;
+  PORT_ADDR_LOW = address & 0xff;
+  delayMicroseconds(3);
+  return PIN_DATA;
+}
+
 
 inline void setDataWriteMode() {
   DDR_DATA = 0xff;
@@ -174,4 +241,76 @@ inline void setDataWriteMode() {
 inline void setDataReadMode() {
   DDR_DATA = 0x00;
   PORT_DATA = 0x00;  // disable pull-up resistors
+}
+
+void readSerialCommand(char *buf, int size) {
+  Serial.setTimeout(1000 * 3600);  // 1 hour
+
+  // Loop until we read something
+  size_t bytesRead = Serial.readBytesUntil('\n', buf, size - 1);
+  // Add null termination, possibly overwriting a trailing carriage return
+  if(bytesRead > 0 && buf[bytesRead-1] == '\r') {
+    buf[bytesRead-1] = '\0';
+  } else {
+    buf[bytesRead] = '\0';
+  }
+}
+
+int findTokenEnd(char *s, int size, int startAfter) {
+  char *end = s + size;
+  char *p = s;
+  if(startAfter > 0) {
+    if(s[startAfter] == '\0') {
+      return startAfter;
+    }
+    p += startAfter + 1;
+  }
+  while(p != end && *p != '\0' && *p != ' ' && *p != '\n') {
+    p++;
+  }
+  return p - s;
+}
+
+void printHelp() {
+  Serial.println("GLS29EE512 programmer");
+  Serial.println("Connect pins a7..a0 to PORTA7..0");
+  Serial.println("Connect pins a15..a8 to PORTC7..0");
+  Serial.println("Connect pins d7..d0 to PORTL7..0");
+  Serial.println("Connect pins WE,CE,OE to PORTG2..0");
+  Serial.println();
+  Serial.println("Commands:");
+  Serial.println("help - print this text");
+  Serial.println("write <page> - write a 128 byte page where <page> is a number between 0 and 511");
+  Serial.println("               inclusive; after the newline, write all 128 data bytes");
+  Serial.println("verify <page> - verify a 128 byte page; after the newline send bytes to verify");
+}
+
+bool parseNumberArgument(char *s, int size, int startAfter, int *resultPtr) {
+  int argEnd = findTokenEnd(s, size, startAfter);
+  if(argEnd <= startAfter + 1) {
+    Serial.println("Missing argument");
+    return false;
+  }
+  char *arg = inputBuffer + startAfter + 1;
+  inputBuffer[argEnd] = '\0';
+  char *parseEnd = NULL;
+  long int result = strtol(arg, &parseEnd, 10);
+  if(parseEnd != inputBuffer + argEnd) {
+    Serial.println("Invalid argument");
+    return false;
+  }
+  *resultPtr = result;
+  return true;
+}
+
+bool verifyPage(uint16_t baseAddr, uint8_t *expected) {
+  setDataReadMode();
+  PORT_CTRL = CHIP_ENABLED | OUTPUT_ENABLED | WRITE_DISABLED;
+  for(int i = 0; i < 128; i++) {
+    uint8_t value = readAddr(baseAddr + i);
+    if(value != expected[i]) {
+      return false;
+    }
+  }
+  return true;
 }
